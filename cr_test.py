68396ab447024e0bec2f4a85b4addbee8f876412
stan-DP <wolf70113@qq.com>
2017-07-10 11:08:50 +0800

    Add files via upload
    1. the crawler can catch the data of the matches on the index page now
    2. add the 'page turning' module, allowing the program to change the index page
    3. the procedure of catching the data of one match is now encapsulated to be a function
    4. bugs fixed

--- cr_test.py
+++ cr_test.py
@@ -1,95 +1,124 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
 from bs4 import BeautifulSoup
 import bs4
 import urllib
 import json
 import os 
 import sys  
+import time
 import json
 reload(sys)  
 sys.setdefaultencoding('utf8')   
 info_Home = {}
 info_Away = {}
 
-#url = 'https://www.whoscored.com/Matches/1080751/Live'
-url = 'https://www.whoscored.com/Matches/1080760/Live/England-Premier-League-2016-2017-Southampton-Leicester'
-driver = webdriver.Chrome()
-driver.get(url)
-data = BeautifulSoup(driver.page_source,"html.parser")
+url = 'https://www.whoscored.com/Regions/252/Tournaments/2/England-Premier-League'
+index_driver = webdriver.Chrome()
+index_driver.set_window_size(1, 1)  
+index_driver.get(url)
+head = 'https://www.whoscored.com'
+index_data = BeautifulSoup(index_driver.page_source,"html.parser")
+table = index_data.find("table", id = "tournament-fixture")
 
-#data = BeautifulSoup(open('d:/test1.html'), "html.parser")
-print type(data)
-team_data = data.find("ul", class_ = "match-centre-stats", attrs = {"data-mode": "team"})
+def get_a_match(match):
+    url = match.get('href')
+    url = head + url
+    driver = webdriver.Chrome()
+    driver.set_window_size(1, 1)  
+    driver.get(url)
+    data = BeautifulSoup(driver.page_source,"html.parser")
+    team_data = data.find("ul", class_ = "match-centre-stats", attrs = {"data-mode": "team"})
 
-x = team_data.find_next("li", attrs = {"data-for": "ratings"})
-info_Home["ratings"] = ((x.find_next("span", attrs = {"data-field": "home"})).string)
-info_Away["ratings"] = ((x.find_next("span", attrs = {"data-field": "away"})).string)
+    x = team_data.find_next("li", attrs = {"data-for": "ratings"})
+    info_Home["ratings"] = ((x.find_next("span", attrs = {"data-field": "home"})).string)
+    info_Away["ratings"] = ((x.find_next("span", attrs = {"data-field": "away"})).string)
 
-x = team_data.find_next("li", attrs = {"data-for": "shotsTotal"})
-info_Home["shots"] = ((x.find_next("span", attrs = {"data-field": "home"})).string)
-info_Away["shots"] = ((x.find_next("span", attrs = {"data-field": "away"})).string)
+    x = team_data.find_next("li", attrs = {"data-for": "shotsTotal"})
+    info_Home["shots"] = ((x.find_next("span", attrs = {"data-field": "home"})).string)
+    info_Away["shots"] = ((x.find_next("span", attrs = {"data-field": "away"})).string)
 
-x = team_data.find_next("li", attrs = {"data-for": "possession"})
-info_Home["possession"] = ((x.find_next("span", attrs = {"data-field": "home"})).string)
-info_Away["possession"] = ((x.find_next("span", attrs = {"data-field": "away"})).string)
+    x = team_data.find_next("li", attrs = {"data-for": "possession"})
+    info_Home["possession"] = ((x.find_next("span", attrs = {"data-field": "home"})).string)
+    info_Away["possession"] = ((x.find_next("span", attrs = {"data-field": "away"})).string)
 
-x = team_data.find_next("li", attrs = {"data-for": "passSuccess"})
-info_Home["pass_success"] = ((x.find_next("span", attrs = {"data-field": "home"})).string)
-info_Away["pass_success"] = ((x.find_next("span", attrs = {"data-field": "away"})).string)
+    x = team_data.find_next("li", attrs = {"data-for": "passSuccess"})
+    info_Home["pass_success"] = ((x.find_next("span", attrs = {"data-field": "home"})).string)
+    info_Away["pass_success"] = ((x.find_next("span", attrs = {"data-field": "away"})).string)
 
-x = team_data.find_next("li", attrs = {"data-for": "dribblesWon"})
-info_Home["dribbles"] = ((x.find_next("span", attrs = {"data-field": "home"})).string)
-info_Away["dribbles"] = ((x.find_next("span", attrs = {"data-field": "away"})).string)
+    x = team_data.find_next("li", attrs = {"data-for": "dribblesWon"})
+    info_Home["dribbles"] = ((x.find_next("span", attrs = {"data-field": "home"})).string)
+    info_Away["dribbles"] = ((x.find_next("span", attrs = {"data-field": "away"})).string)
 
-x = team_data.find_next("li", attrs = {"data-for": "aerialsWon"})
-info_Home["aerials_won"] = ((x.find_next("span", attrs = {"data-field": "home"})).string)
-info_Away["aerials_won"] = ((x.find_next("span", attrs = {"data-field": "away"})).string)
+    x = team_data.find_next("li", attrs = {"data-for": "aerialsWon"})
+    info_Home["aerials_won"] = ((x.find_next("span", attrs = {"data-field": "home"})).string)
+    info_Away["aerials_won"] = ((x.find_next("span", attrs = {"data-field": "away"})).string)
 
-x = team_data.find_next("li", attrs = {"data-for": "tackleSuccessful"})
-info_Home["tackles"] = ((x.find_next("span", attrs = {"data-field": "home"})).string)
-info_Away["tackles"] = ((x.find_next("span", attrs = {"data-field": "away"})).string)
+    x = team_data.find_next("li", attrs = {"data-for": "tackleSuccessful"})
+    info_Home["tackles"] = ((x.find_next("span", attrs = {"data-field": "home"})).string)
+    info_Away["tackles"] = ((x.find_next("span", attrs = {"data-field": "away"})).string)
 
-x = team_data.find_next("li", attrs = {"data-for": "cornersTotal"})
-info_Home["corners"] = ((x.find_next("span", attrs = {"data-field": "home"})).string)
-info_Away["corners"] = ((x.find_next("span", attrs = {"data-field": "away"})).string)
+    x = team_data.find_next("li", attrs = {"data-for": "cornersTotal"})
+    info_Home["corners"] = ((x.find_next("span", attrs = {"data-field": "home"})).string)
+    info_Away["corners"] = ((x.find_next("span", attrs = {"data-field": "away"})).string)
 
-x = team_data.find_next("li", attrs = {"data-for": "dispossessed"})
-info_Home["dispossessed"] = ((x.find_next("span", attrs = {"data-field": "home"})).string)
-info_Away["dispossessed"] = ((x.find_next("span", attrs = {"data-field": "away"})).string)
+    x = team_data.find_next("li", attrs = {"data-for": "dispossessed"})
+    info_Home["dispossessed"] = ((x.find_next("span", attrs = {"data-field": "home"})).string)
+    info_Away["dispossessed"] = ((x.find_next("span", attrs = {"data-field": "away"})).string)
 
-x = data.find("div", class_ = "match-header", id = "match-header")
-y = x.find_next("tr")
-home_team = ((y.find_next("a")).string)
-x = y.find_next("a")
-z = x.find_parent("td")
-x = z.find_next_sibling("td")
-result = x.string
-z = x.find_next("a")
-away_team = z.string
-x = y.find_next_sibling("tr")
-y = x.find_next("td")
-z = y.find_next_sibling("td")
-x = z.find_next("div")
-y = x.find_next_sibling("div")
-x = y.find_next_sibling("div")
-y = x.find_next("dd")
-x = y.find_next_sibling("dd")
-date = x.string
+    x = data.find("div", class_ = "match-header", id = "match-header")
+    y = x.find_next("tr")
+    home_team = ((y.find_next("a")).string)
+    x = y.find_next("a")
+    z = x.find_parent("td")
+    x = z.find_next_sibling("td")
+    result = x.string
+    z = x.find_next("a")
+    away_team = z.string
+    x = y.find_next_sibling("tr")
+    y = x.find_next("td")
+    z = y.find_next_sibling("td")
+    x = z.find_next("div")
+    y = x.find_next_sibling("div")
+    x = y.find_next_sibling("div")
+    y = x.find_next("dd")
+    x = y.find_next_sibling("dd")
+    date = x.string
 
-fo = open("output.txt", "a+")
-list = {}
-data = {}
-list["home"]=info_Home
-list["away"]=info_Away
-data["date"] = date
-data["home"] = home_team
-data["away"] = away_team
-data["result"] = result
-data["list"] = list
-jsonStr = json.dumps(data)  
-fo.write("jsonStr:")
-fo.write(jsonStr)
-fo.write('\n')
+    fo = open("output.txt", "a+")
+    list = {}
+    data = {}
+    list["home"]=info_Home
+    list["away"]=info_Away
+    data["date"] = date
+    data["home"] = home_team
+    data["away"] = away_team
+    data["result"] = result
+    data["list"] = list
+    jsonStr = json.dumps(data)  
+    fo.write("jsonStr:")
+    fo.write(jsonStr)
+    fo.write('\n')
+    driver.close()
+    print 'get\n'
+
+for match in table.find_all_next("a", class_ = "result-1 rc"):
+    print match
+    get_a_match(match)
+
+for i in range(1, 6):
+    actions = ActionChains(index_driver)
+    element = index_driver.find_element_by_css_selector("a.previous.button.ui-state-default.rc-l.is-default")
+    actions.move_to_element(element)
+    time.sleep(1)
+    actions.click().perform()
+    time.sleep(1)
+    index_data = BeautifulSoup(index_driver.page_source,"html.parser")
+    table = index_data.find("table", id = "tournament-fixture")
+    for match in table.find_all_next("a", class_ = "result-1 rc"):
+        print match
+        get_a_match(match)
 
 #fo = open("output.txt", "w")
 #fo.write(data.prettify())
